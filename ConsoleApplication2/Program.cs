e0e1cbc34276eaff05ce121673941cf99ee4265c
marcjonesuk <jones.marc@gmail.com>
2017-02-20 17:41:52 +0000

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,7 +1,9 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
+using RedisLib2;
 using StackExchange.Redis;
 using System;
 using System.Collections.Concurrent;
+using System.Data.OleDb;
 using System.Diagnostics;
 using System.Dynamic;
 using System.Threading;
@@ -9,84 +11,15 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
-    public class RedisQueue {
-        private int _batchSize = 500;
-        private BlockingCollection<string> _bc = new BlockingCollection<string>();
-        private LoadedLuaScript loaded;
-        private IDatabase _db;
-        private string _queue;
-        private bool _running;
-        public long Length;
-
-        public RedisQueue(IDatabase db, IServer server, string queueName)
-        {
-            _db = db;
-            _queue = queueName;
-            const string Script = @"local result = redis.call('lrange', @key, 0, @batchSize - 1)
-                                    redis.call('ltrim', @key, @batchSize, -1)
-                                    return result";
-            var prepared = LuaScript.Prepare(Script);
-            loaded = prepared.Load(server);
-        }
-
-        public void Subscribe(Action<string> action)
-        {
-            if (_running)
-                throw new InvalidOperationException("Already processing queue");
-
-            _running = true;
-            Task.Run(() =>
-            {
-                while (!_bc.IsCompleted)
-                {
-                    string data = null;
-                    try
-                    {
-                        data = _bc.Take();
-                    }
-                    catch (InvalidOperationException) { }
-                    if (data != null)
-                    {
-                        action(data);
-                    }
-                }
-            });
-
-            Task.Run(async () =>
-            {
-                while (_running)
-                {
-                    try
-                    {
-                        var result = await _db.ScriptEvaluateAsync(loaded, new { key = _queue, batchSize = _batchSize }).ConfigureAwait(false);
-                       // Length = await _db.ListLengthAsync(_queue).ConfigureAwait(false);
-                        var range = (RedisValue[])result;
-                        foreach (var r in range)
-                            _bc.Add(r);
-
-                        if (range.Length == 0)
-                            Thread.Sleep(1);
-                    }
-                    catch(Exception e)
-                    {
-                        Console.WriteLine(e);
-                    }
-                }
-                _bc.CompleteAdding();
-            });
-        }
-
-        public void Stop()
-        {
-            _running = false;
-        }
-    }
-
     class Program
     {
         static void StartConsumer(ConnectionMultiplexer cm, string q)
         {
-            var queue = new RedisQueue(cm.GetDatabase(), cm.GetServer("localhost:6379"), q);
+            //DDBRDS001.spreadex.com:6381,password=DEV_bc7859c63ce32c5f6636717d9068f234bf4095eaeeff86b08d480396648bfe21
+            //var server = cm.GetServer("DDBRDS001.spreadex.com:6381");
+            var server = cm.GetServer("localhost:6379");
+
+            var queue = new RedisQueue(cm.GetDatabase(), server, q);
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
@@ -99,7 +32,6 @@ namespace ConsoleApplication2
                 dynamic data = JsonConvert.DeserializeObject<ExpandoObject>(a);
                 if (last != null && data.Count - 1 != last)
                 {
-
                 }
                 b++;
                 if (sw.ElapsedMilliseconds > 1000)
@@ -110,16 +42,66 @@ namespace ConsoleApplication2
                     sw.Start();
                     b = 0;
                 }
+
                 last = data.Count;
+            });
+        }
+
+        static void StartCircularConsumer(ConnectionMultiplexer cm, string q)
+        {
+            //DDBRDS001.spreadex.com:6381,password=DEV_bc7859c63ce32c5f6636717d9068f234bf4095eaeeff86b08d480396648bfe21
+            //var server = cm.GetServer("DDBRDS001.spreadex.com:6381");
+            var server = cm.GetServer("localhost:6379");
+            var queue = new RedisCircularBuffer(cm.GetDatabase(), server, 1000000, q, "consumer1");
+
+            long old = -1;
+            bool hadone = false;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var count = 0;
+            var c2 = 0;
+
+            queue.Subscribe((a) =>
+            {
+                count++;
+                c2++;
+                var num = long.Parse(a);
+
+                if (hadone && num != old + 1)
+                    Console.WriteLine("error");
+
+                old = num;
+                hadone = true;
+                //Thread.Sleep(1);
+                if (sw.ElapsedMilliseconds > 1000)
+                {
+                    var amount = (count * 1000) / sw.ElapsedMilliseconds;
+                    Console.WriteLine("\r" + amount + "             " + queue.LocalQueueSize);
+                    count = 0;
+                    sw.Reset();
+                    sw.Start();
+                }
+
+                if (c2 % 100000 == 0)
+                {
+                    Console.WriteLine(c2);
+                    Console.WriteLine(a);
+                }
             });
         }
 
         static void Main(string[] args)
         {
-            ConnectionMultiplexer cm = ConnectionMultiplexer.Connect("localhost");
+            Thread.Sleep(2000);
+            //ConnectionMultiplexer cm = ConnectionMultiplexer.Connect("DDBRDS001.spreadex.com:6381,password=DEV_bc7859c63ce32c5f6636717d9068f234bf4095eaeeff86b08d480396648bfe21");
+            ConnectionMultiplexer cm = ConnectionMultiplexer.Connect("localhost:6379");
 
-           StartConsumer(cm, "test1");
-           //StartConsumer(cm, "test2");
+            StartCircularConsumer(cm, "testbuffer");
+
+            //StartConsumer(cm, "test1");
+            //StartConsumer(cm, "test2");
             //StartConsumer(cm, "test3");
             //StartConsumer(cm, "test4");
 
